0e7fcc3ca7a5a6de3f8dd9a542c7653769688f56
Adam Carter <acarteas@users.noreply.github.com>
2013-06-11 22:31:34 +0000

    -switched email SMTP settings. -Added SendGrid libraries (for SMTP)

--- Source/OSBIDE/Source/OSBIDE.Web/Models/Email.cs
+++ Source/OSBIDE/Source/OSBIDE.Web/Models/Email.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using SendGridMail;
+using SendGridMail.Transport;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Web;
 
@@ -21,21 +24,25 @@ namespace OSBIDE.Web.Models
             SmtpClient mailClient = new SmtpClient();
 
             MailAddress fromAddress = new MailAddress(ConfigurationManager.AppSettings["OsbideFromEmail"], "OSBIDE");
-
+            var credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"], ConfigurationManager.AppSettings["EmailPassword"]);
+            var transportSMTP = SMTP.GetInstance(credentials); 
             foreach (MailAddress recipient in to)
             {
-                MailMessage mm = new MailMessage();
-
-                mm.From = fromAddress;
-                mm.To.Add(recipient);
-                mm.Subject = subject;
-                mm.Body = message;
-                mm.IsBodyHtml = true;
+                SendGrid gridMessage = SendGrid.GetInstance(
+                    fromAddress, 
+                    new MailAddress[] { recipient }, 
+                    new MailAddress[0], 
+                    new MailAddress[0], 
+                    subject, 
+                    message, 
+                    message
+                    );
+                
 
                 //bomb's away!
-                mailClient.Send(mm);
+                transportSMTP.Deliver(gridMessage);
             }
-            
+
             mailClient.Dispose();
 #endif
         }
