94b5b01ad8c02445bba406fa6db9e6afc96cd620
Adam Carter <acarteas@users.noreply.github.com>
2013-07-30 21:23:04 +0000

    -Added new plugin folder (will move next checkin)

---
+++ Source/OSBIDE.Plugins.VS2012/OSBIDE.Plugins.VS2012/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace WashingtonStateUniversity.OSBIDE_Plugins_VS2012
+{
+    static class GuidList
+    {
+        public const string guidOSBIDE_Plugins_VS2012PkgString = "d36f74b0-6e55-4872-8696-0ca1d5a2203e";
+        public const string guidOSBIDE_Plugins_VS2012CmdSetString = "8917860c-7c5d-4f90-a9dc-db2fb42ddf67";
+        public const string guidToolWindowPersistanceString = "e592919c-94f6-427b-bd12-6cb46440bc3a";
+
+        public static readonly Guid guidOSBIDE_Plugins_VS2012CmdSet = new Guid(guidOSBIDE_Plugins_VS2012CmdSetString);
+    };
+}
---
+++ Source/OSBIDE.Plugins.VS2012/OSBIDE.Plugins.VS2012/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace WashingtonStateUniversity.OSBIDE_Plugins_VS2012
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMyCommand =        0x100;
+        public const uint cmdidMyTool =    0x101;
+
+    };
+}
