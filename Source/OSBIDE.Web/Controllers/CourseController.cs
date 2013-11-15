e7477511317a1cfd6c025beedd13f77bcbe5fe6e
Adam Carter <acarteas@users.noreply.github.com>
2013-11-15 18:52:46 +0000

    -work on course selection page

--- Source/OSBIDE.Web/Controllers/CourseController.cs
+++ Source/OSBIDE.Web/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 ﻿using OSBIDE.Library.Models;
+using OSBIDE.Web.Models.Attributes;
 using OSBIDE.Web.Models.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Web.Mvc;
 
 namespace OSBIDE.Web.Controllers
 {
+    [OsbideAuthorize]
     public class CourseController : ControllerBase
     {
         //
@@ -21,6 +23,38 @@ namespace OSBIDE.Web.Controllers
         public ActionResult MyCourses()
         {
             return View();
+        }
+
+        public JsonResult GetAllCourses()
+        {
+            CoursesViewModel vm = new CoursesViewModel();
+            vm.AllCourses = Db.Courses
+                .Where(c => c.SchoolId == CurrentUser.SchoolId)
+                .OrderBy(c => c.Prefix)
+                .ToList();
+            vm.CurrentUser = CurrentUser;
+
+            foreach (Course course in vm.AllCourses)
+            {
+                if (vm.CoursesByPrefix.ContainsKey(course.Prefix) == false)
+                {
+                    vm.CoursesByPrefix.Add(course.Prefix, new SortedDictionary<string, Course>());
+                }
+                vm.CoursesByPrefix[course.Prefix].Add(course.CourseNumber, course);
+            }
+            var simpleCourse = vm.CoursesByPrefix.Select(c => new
+            {
+                Prefix = c.Key,
+                Courses = c.Value.Select(
+                    co => new
+                    {
+                        Prefix = c.Key,
+                        CourseNumber = co.Value.CourseNumber,
+                        Description = co.Value.Description
+                    }
+                )
+            });
+            return this.Json(simpleCourse, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Search()
