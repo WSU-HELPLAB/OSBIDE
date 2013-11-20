1b0d7e61e79445a0570b10f802f3d73a1c7bac7a
Adam Carter <acarteas@users.noreply.github.com>
2013-11-20 17:03:34 +0000

    -copied over OSBLE's old filesystem architecture for use in OSBIDE courses.

---
+++ Source/OSBIDE.Web/FileSystem/SubmissionFilePath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace OSBIDE.Web.FileSystem
+{
+    public class SubmissionFilePath : FileSystemBase
+    {
+        private int _teamID;
+        private string _submissionPrefix = "Submissions";
+
+        public SubmissionFilePath(IFileSystem pathBuilder, int teamID)
+            : base(pathBuilder)
+        {
+            _teamID = teamID;
+        }
+
+        public override string GetPath()
+        {
+            string returnPath = Path.Combine(PathBuilder.GetPath(), _submissionPrefix, _teamID.ToString());
+            return returnPath;
+        }
+    }
+}
