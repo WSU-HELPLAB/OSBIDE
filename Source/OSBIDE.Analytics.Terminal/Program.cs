049b616a4ce866c17886a8426f6c8c926ccb9330
Adam Carter <acarteas@users.noreply.github.com>
2014-07-29 23:23:49 +0000

    -Added comment metrics to analytics project.

--- Source/OSBIDE.Analytics.Terminal/Program.cs
+++ Source/OSBIDE.Analytics.Terminal/Program.cs
@@ -4,15 +4,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OSBIDE.Analytics.Terminal.Views;
 
 namespace OSBIDE.Analytics.Terminal
 {
     class Program
     {
+        private enum MenuOption { CommentMetrics = 1, Exit }
         static void Main(string[] args)
         {
-            SessionMetricsView metrics = new SessionMetricsView();
-            metrics.Run();
+
+            int userChoice = 0;
+            while (userChoice != (int)MenuOption.Exit)
+            {
+                Console.WriteLine("*** OSBIDE Analytics ***");
+                Console.WriteLine((int)MenuOption.CommentMetrics + ". Run comment metrics");
+                Console.WriteLine((int)MenuOption.Exit + ". Exit");
+                Console.Write(">> ");
+
+                string rawInput = Console.ReadLine();
+                if (Int32.TryParse(rawInput, out userChoice) == false)
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+                else
+                {
+                    MenuOption selection = (MenuOption)userChoice;
+                    switch (selection)
+                    {
+                        case MenuOption.CommentMetrics:
+                            CommentMetricsView view = new CommentMetricsView();
+                            view.Run();
+                            break;
+                        case MenuOption.Exit:
+                            Console.WriteLine("Exiting program.");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
     }
 }
